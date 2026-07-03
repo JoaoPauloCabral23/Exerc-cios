d9b61e7b767838b0dd1ab69dbf8075bc0ad6b956
JOÃO PAULO CABRAL CONTENTE <joao_contente@estudante.sesisenai.org.br>
2026-07-03 16:25:31 -0300

    Criação do Exercício 5 de JS

---
+++ ExercícioJs/atividade5.js
@@ -0,0 +1,5 @@
+const primeiroNome = "João";                   // Utilizar o _ está incorreto.
+const dataNascimento = "01/01/2000";           // Deve começar com letra minúscula.
+const salario = 3000;                          // Deveria ser tudo minúsculo.
+let numeroDeTentativas = 0;                    // Deveria a cada palavra diferente ter a primeira letra maiúscula.
+const nomeCompleto = "João Silva";             // Deveria começar com a letra minúscula e na próxima palavra ter a maiúscula.
