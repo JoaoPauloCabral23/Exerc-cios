d4ada1d42cc70310794def975c9678e15dbef77e
JOÃO PAULO CABRAL CONTENTE <joao_contente@estudante.sesisenai.org.br>
2026-07-03 16:17:21 -0300

    Criação do Exercício 4 de JS

---
+++ ExercícioJs/atividade4.js
@@ -0,0 +1,6 @@
+console.log(5 == "5");                  //true      pois o 5 realmente é igual a 5.
+console.log(5 === "5");                 //false     pois o 5 até é 5, mas seu tipo é diferente.
+console.log(0 == false);                //true      pois o 0 é a mesma coisa que false por não ser nada.
+console.log(0 === false);               //false     pois eles são de tipos diferentes.
+console.log(null == undefined);         //true      pois ambos são sem valor.
+console.log(null === undefined);        //false     pois eles são de tipos diferentes.
