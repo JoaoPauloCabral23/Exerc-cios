432a2db2be75ebd050ea4641e2d6217817100ce1
JOÃO PAULO CABRAL CONTENTE <joao_contente@estudante.sesisenai.org.br>
2026-07-03 15:52:22 -0300

    Criação do Exercício 1 de JS

---
+++ ExercícioJs/atividade1.js
@@ -0,0 +1,9 @@
+const nomeUm = "Ana";
+const nomeDois = "Carlos";
+
+let idadeUm = 25;
+let idadeDois = 30;
+
+console.log(nomeDois, idadeDois);
+
+// As variaveis estavam iguais, tanto as de "nome" quanto as de "idade", elas devem ser diferentes para imprimir um valor específico.
