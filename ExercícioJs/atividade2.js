3f210c7a7d3100ba96ba2efff1394afaca83a2c5
JOÃO PAULO CABRAL CONTENTE <joao_contente@estudante.sesisenai.org.br>
2026-07-03 16:07:16 -0300

    Criação do Exercício 2 de JS

---
+++ ExercícioJs/atividade2.js
@@ -0,0 +1,11 @@
+let int = 20;
+let dec = 12.1;
+let nome = "João Paulo Cabral Contente";
+let booleano = true;
+let undefined
+
+console.log(int);
+console.log(dec);
+console.log(nome);
+console.log(booleano);
+console.log(undefined);
