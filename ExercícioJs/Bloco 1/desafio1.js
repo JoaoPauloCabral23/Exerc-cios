9ee58589c35e97b703450c1405ab7c6fa14d6912
JOÃO PAULO CABRAL CONTENTE <joao_contente@estudante.sesisenai.org.br>
2026-07-03 16:49:00 -0300

    Criação do Desafio 1 de JS

---
+++ ExercícioJs/Bloco 1/desafio1.js
@@ -0,0 +1,8 @@
+const nome = "Jopacaco";
+let classe = "Assasino";
+let nivel = 100;
+let pontosDeVida = 10000;
+let vivo = true;
+let armaEquipada = "Katana Blackthorn";
+
+console.log(`${nome} é um ${classe} nível ${nivel} com ${pontosDeVida} HP. Arma: ${armaEquipada}. Status: ${vivo}`);
