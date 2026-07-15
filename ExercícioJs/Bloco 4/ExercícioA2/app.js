a3bb6e249db57ece95f6b8a9539c8b4b225ee7c6
JOÃO PAULO CABRAL CONTENTE <joao_contente@estudante.sesisenai.org.br>
2026-07-15 14:57:11 -0300

    Criação do Exercício A2 do Bloco 4 de JS

---
+++ ExercícioJs/Bloco 4/ExercícioA2/app.js
@@ -0,0 +1,7 @@
+const titulo = document.querySelector("#titulo");
+const subtitulo = document.querySelector("#subtitulo");
+const caixa = document.querySelector("#caixa");
+
+titulo.textContent = "JavaScript no DOM!";
+subtitulo.textContent = "João Paulo Cabral Contente";
+caixa.innerHTML = "<strong>Conteúdo alterado</strong>";
