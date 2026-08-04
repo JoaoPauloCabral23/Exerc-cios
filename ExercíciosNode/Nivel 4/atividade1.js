10d411b444a8784d023d8e3b892ee7961b453bee
JOÃO PAULO CABRAL CONTENTE <joao_contente@estudante.sesisenai.org.br>
2026-08-04 17:19:38 -0300

    Criação da atividade 1 do Nivel 4 de Node

---
+++ ExercíciosNode/Nivel 4/atividade1.js
@@ -0,0 +1,8 @@
+let celsius = 23;
+
+fahrenheit = (celsius * 9/5) + 32
+kelvin = celsius + 273.15;
+
+console.log(`Celsius: ${celsius}`);
+console.log(`Fahrenheit: ${fahrenheit}`);
+console.log(`Kelvin: ${kelvin}`);
