c8b4f635c33c71dd8336a47c9820952ad4ca386e
JOÃO PAULO CABRAL CONTENTE <joao_contente@estudante.sesisenai.org.br>
2026-08-04 16:13:47 -0300

    Criação da atividade 2 do Exercício 2 de Node

---
+++ ExercíciosNode/Nivel 2/atividade2.js
@@ -0,0 +1,7 @@
+let numero1 = Number(process.argv[2]);
+let numero2 = Number(process.argv[3]);
+
+console.log(numero1 + numero2);
+console.log(numero1 - numero2);
+console.log(numero1 * numero2);
+console.log(numero1 / numero2);
