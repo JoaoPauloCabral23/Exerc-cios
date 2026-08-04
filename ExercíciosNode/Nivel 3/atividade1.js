3454d4bd7a7bba2f74ac5d88c4fb66ef4c6fe2ef
JOÃO PAULO CABRAL CONTENTE <joao_contente@estudante.sesisenai.org.br>
2026-08-04 16:50:04 -0300

    Criação da atividade 1 e 2 do Nivel 3 de Node

---
+++ ExercíciosNode/Nivel 3/atividade1.js
@@ -0,0 +1,9 @@
+let nivel = 5;
+
+if (nivel <= 10) {
+    console.log("Iniciante");
+} else if (nivel >= 31){
+    console.log("Veterano");
+} else {
+    console.log("Intermediário");
+}
---
+++ ExercíciosNode/Nivel 3/atividade2.js
@@ -0,0 +1,5 @@
+let nivel = 5;
+XP = nivel * 500;
+
+console.log(`Nivel atual: ${nivel}`);
+console.log(`XP para o próximo nível: ${XP}`);
