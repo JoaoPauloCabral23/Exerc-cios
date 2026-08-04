f2349d9856274cca19bacfcadad92c058425eef5
JOÃO PAULO CABRAL CONTENTE <joao_contente@estudante.sesisenai.org.br>
2026-08-04 16:03:45 -0300

    Criação do Exercício 3 de Node

---
+++ ExercíciosNode/atividade3.js
@@ -0,0 +1,11 @@
+let nome = "Jopacaco";
+let classe = "Assasino";
+let nivel = 100;
+let vida = 1000;
+let mana = 500;
+
+console.log(`Nome: ${nome}`);
+console.log(`Classe: ${classe}`);
+console.log(`Nível: ${nivel}`);
+console.log(`Vida: ${vida}`);
+console.log(`Mana: ${mana}`);
